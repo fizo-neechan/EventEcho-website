062738692febe85db72aa4aad4c739b32e96072d
kellyy8 <kellyyu2886@gmail.com>
2024-04-14 00:11:20 -0700

    Implemented round block with Instagram + App store links.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Home from './pages/Home';
 import About from './pages/About';
 import logo from './images/logo_navbar.png';
+import icon_ig from './images/icon_ig.png';
+import icon_app_store from './images/icon_app_store.png';
 import {BrowserRouter, Routes, Route, Link, NavLink} from 'react-router-dom';
 
 function App() {
@@ -17,6 +19,11 @@ function App() {
         <NavLink to="/about" className="navbarItem"> ABOUT </NavLink>
       </div>
     </nav>
+
+    <div className="externalLinkWrapper">
+      <div> <Link to="https://www.instagram.com/eventechoucla"> <img src={icon_ig} alt="Instagram logo" className="externalLinkItems"/> </Link> </div>
+      <div> <Link to="https://www.instagram.com/eventechoucla"> <img src={icon_app_store} alt="App Store logo" className="externalLinkItems"/> </Link> </div>
+    </div>
 
     <Routes>
       <Route path="/" element={<Home/>} />
--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -1,11 +1,12 @@
 import '../App.css';
+import './Home.css';
 
 function Home() {
   return (
     <div>
         <p> HOME PAGE </p> 
     </div>
-);
+  );
 }
 
 export default Home;
