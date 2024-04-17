bbc05c3b8808ced8a4a6c95bf7ca7c6d9851ee6f
Ben Liang <benliang03@gmail.com>
2024-04-16 23:35:47 -0700

    Added Home and About pages

--- my-app/src/pages/About.js
+++ my-app/src/pages/About.js
@@ -1,9 +1,25 @@
 import '../App.css';
+import './About.css';
+import about_back1 from '../images/about_back1.png'
+//import about_back2 from '../images/about_back2.png'
 
 function About() {
   return (
-    <div>
-        <p> ABOUT PAGE </p> 
+    <div className="about">
+        <img src={about_back1} alt=""/>
+        <p className="text">
+        ARE YOU LOOKING FOR CONCERT BUDDIES?<br/>
+        <br/>
+        WANT TO BUY/SELL CONCERT TIX?<br/>
+        EVENTECHO IS THE PERFECT PRODUCT TO AMPLIFY<br/>
+        YOUR SOCIAL EXPERIENCE!<br/>
+        <br/>
+        COMING TO THE APPLE STORE IN THE SPRING!<br/>
+        <br/>
+        IN THE MEAN TIME, FOLLOW US ON INSTAGRAM AND<br/>
+        FILL OUT <a href="https://forms.gle/kcWvPBU47DkwAcFf6">THIS FORM </a>
+        SO THAT WE CAN CONNECT <br/> YOU W/ OTHER BRUINS<br/>
+        </p>
     </div>
 );
 }
--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -1,10 +1,19 @@
 import '../App.css';
 import './Home.css';
+import back1 from '../images/home_back1.png'
+import back2 from '../images/home_back2.png'
 
 function Home() {
   return (
-    <div>
-        <p> HOME PAGE </p> 
+    <div className="home">
+        <img src={back1} alt=""/>
+        <img src={back2} alt=""/> 
+        <p className="text">
+            A CENTRALIZED PLATFORM<br/>
+            TO FIND CONCERT COMPANIONS<br/>
+            & BUYING/SELLING<br/>
+            TICKETS FOR UCLA STUDENTS<br/>
+        </p>
     </div>
   );
 }
