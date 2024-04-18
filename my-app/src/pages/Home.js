7bb9d5e79cef74b61b1cd593a38a1590ea8ce85e
Meagan Clarke <megclarke@g.ucla.edu>
2024-04-18 03:06:52 -0700

    added tagline below pictures, left aligned & decreased size of text

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -4,6 +4,7 @@ import back1 from '../images/home_back1.png'
 import back2 from '../images/home_back2.png'
 import phone_back1 from '../images/full_circle_bg.png'
 import phone_back2 from '../images/iphone_fullsize.png'
+import eventecho from '../images/eventecho.png'
 
 function Home() {
   return (
@@ -15,6 +16,7 @@ function Home() {
             {/*<img src={phone_back2} className="smallscreen-img" alt=""/>*/}
         </div> 
         <div>
+            <img src={eventecho} className="" alt=""/>
             <p className="text">
                 A CENTRALIZED PLATFORM<br/>
                 TO FIND CONCERT COMPANIONS<br/>
