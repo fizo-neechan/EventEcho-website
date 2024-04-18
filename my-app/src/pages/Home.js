7beb57b9a69ec63b3e5f5fdc24d342273f118c81
Meagan Clarke <megclarke@g.ucla.edu>
2024-04-18 02:45:47 -0700

    an attempt to make the images responsive. the images aren't overlapping properly. will try again once I get a single image from designers.

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -2,13 +2,17 @@ import '../App.css';
 import './Home.css';
 import back1 from '../images/home_back1.png'
 import back2 from '../images/home_back2.png'
+import phone_back1 from '../images/full_circle_bg.png'
+import phone_back2 from '../images/iphone_fullsize.png'
 
 function Home() {
   return (
     <div className="home">
         <div>
-            <img src={back1} alt=""/>
-            <img src={back2} alt=""/>
+            <img src={back1} className="fullscreen-img" alt=""/>
+            <img src={back2} className="fullscreen-img" alt=""/>
+            <img src={phone_back1} className="smallscreen-img" alt=""/>
+            {/*<img src={phone_back2} className="smallscreen-img" alt=""/>*/}
         </div> 
         <div>
             <p className="text">
